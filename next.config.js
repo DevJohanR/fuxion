e362e55a1f0e60c2b940a9c594f279dd7d3eacd0
Johan Riascos <desarrollador7777@gmail.com>
2024-07-19 13:00:42 -0500

    primer backup

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // Elimina o comenta la línea 'appDir' si no es necesaria
+    // appDir: true,
+  },
+}
+
+module.exports = nextConfig;
